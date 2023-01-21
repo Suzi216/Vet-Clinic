47845ec1b5711e4f075d12442c8b45722c41f422
suzana marsela <66145033+Suzi216@users.noreply.github.com>
2023-01-21 20:29:05 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -18,3 +18,8 @@ CREATE TABLE species (id BIGSERIAL PRIMARY KEY, name VARCHAR);
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id  INT references species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT references owners(id);
+
+
+CREATE table vets(id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(150), age integer, date_of_graduation DATE);
+CREATE TABLE specializations(species_id INT, vets_id INT, PRIMARY KEY(species_id), CONSTRAINT species_id  FOREIGN KEY(species_id) references species(id), CONSTRAINT vets_id FOREIGN KEY(species_id) REFERENCES vets(id));
+CREATE TABLE visits(animals_id INTEGER NOT NULL, vets_id INTEGER NOT NULL, date_of_visit DATE, primary key(animals_id, vets_id, date_of_visit), CONSTRAINT animals_id FOREIGN KEY(animals_id) REFERENCES animals(id), CONSTRAINT vets_id FOREIGN KEY(vets_id) REFERENCES vets(id));
