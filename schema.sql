ab38c579ecce89182a3f7c6142758ed757184fa6
suzana marsela <66145033+Suzi216@users.noreply.github.com>
2023-01-21 16:10:24 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,10 @@ create table animals(
 );
 
 ALTER TABLE animals ADD species varchar(150);
+
+CREATE TABLE owners (id BIGSERIAL PRIMARY KEY, full_name VARCHAR(150), age INT);
+CREATE TABLE species (id BIGSERIAL PRIMARY KEY, name VARCHAR);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id  INT references species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT references owners(id);
